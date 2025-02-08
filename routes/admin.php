6e7675955fd39c1d480065920ff38a0c3537b01f
mosama <mosama88@mail.com>
2025-02-08 14:14:22 +0200

    dONE branch26

--- routes/admin.php
+++ routes/admin.php
@@ -8,7 +8,6 @@ use App\Http\Controllers\Dashboard\VacationController;
 
 
 Route::middleware(['auth:admin'])->name('dashboard.')->group(function () {
-    // Route::resource('/branches', BranchController::class);
     Route::view('/branches', 'dashboard.branches.index')->name('branchess');
     Route::view('/jobGrades', 'dashboard.jobGrades.index')->name('jobGrades');
     Route::resource('/employees', EmployeeController::class);
