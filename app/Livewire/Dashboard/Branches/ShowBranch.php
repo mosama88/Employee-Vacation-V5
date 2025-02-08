78aaea8ab8e15d9b3ef6026f4b1ebc91b5934e70
mosama <mosama88@mail.com>
2025-02-08 11:15:59 +0200

    Done Edit2

---
+++ app/Livewire/Dashboard/Branches/ShowBranch.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Dashboard\Branches;
+
+use Livewire\Component;
+
+class ShowBranch extends Component
+{
+    public function render()
+    {
+        return view('dashboard.branches.show-branch');
+    }
+}
