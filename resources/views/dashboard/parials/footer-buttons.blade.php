87e3498eb919c0a299c197c7aa8bb4e76ac3e395
mosama <mosama88@mail.com>
2025-02-09 14:09:19 +0200

    Done Go5

--- resources/views/dashboard/parials/footer-buttons.blade.php
+++ resources/views/dashboard/parials/footer-buttons.blade.php
@@ -1,6 +1,4 @@
 <div class="modal-footer mx-auto">
     <x-adminlte-button class="btn-flat" type="submit" label="{{ $submitName }}" theme="{{ $btnColor }}"
         icon="fas fa-lg fa-save" />
-
-
 </div>
